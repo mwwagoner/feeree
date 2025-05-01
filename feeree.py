6b3533f14a5074bf0f1e890a47bda801e79b6617
Michael Wagoner <mwwagoner@gmail.com>
2025-05-01 15:05:54 -0400

    Initial Commit

---
+++ feeree.py
@@ -0,0 +1,29 @@
+import feedparser
+
+urls = ['https://thefp.com/feed', 'https://www.wired.com/feed/rss', 'https://standardebooks.org/feeds/rss/new-releases'] 
+
+def getfeeds(feedurl):
+    
+    d = feedparser.parse(feedurl)
+    entrycount = 0
+    
+    print('--------------------')
+    print(d['feed']['title'])
+    print(d['feed']['date'])
+    print("")
+    
+    for entry in d['entries']:
+        print(str(entrycount) + ") " + entry['title'])
+        print(entry['published'])
+        print(entry['link'])
+        print("---")
+        entrycount += 1
+    
+    print(d['entries'][0].keys())
+
+def main():
+    for url in urls:
+        getfeeds(url)
+
+if __name__ == '__main__':
+    main()
