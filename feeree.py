d71f7f1701b50ff067eab877bf56a953bbee47b8
Michael Wagoner <mwwagoner@gmail.com>
2025-05-03 19:35:50 -0400

    Modifies getfeed to return a list of lines

--- feeree.py
+++ feeree.py
@@ -1,11 +1,12 @@
 import feedparser
 # TODO: Add a way to save individual entries from a feed to revisit later
-urls = ['https://thefp.com/feed', 'https://www.wired.com/feed/rss', 'https://standardebooks.org/feeds/rss/new-releases'] 
+# TODO: Check for a valid url before trying to get a feed
 
-def getfeeds(feedurl):
+def getfeed(feedurl):
     
     d = feedparser.parse(feedurl)
     entrycount = 0
+    items = []
     
     print('--------------------')
     print(d['feed']['title'])
@@ -13,18 +14,34 @@ def getfeeds(feedurl):
     print("")
     
     for entry in d['entries']:
-        print(str(entrycount) + ") " + entry['title'])
-        print(entry['published'])
-        print(entry['link'])
-        print(entry['summary'])
-        print("---")
+        #print(str(entrycount) + ") " + entry['title'])
+        items.append(str(entrycount) + ") " + entry['title'])
+        #print(entry['published'])
+        items.append(entry['published'])
+        #print(entry['link'])
+        items.append(entry['link'])
+        #print(entry['summary'])
+        items.append(entry['summary'])
+        #print("---")
         entrycount += 1
+
+    #print(items)
+    return items
     
-    print(d['entries'][0].keys())
+    #print(d['entries'][0].keys())
 
 def main():
+    urls = ['https://thefp.com/feed', 
+            'https://www.wired.com/feed/rss', 
+            'https://standardebooks.org/feeds/rss/new-releases'] 
+    feeds = []
+
     for url in urls:
-        getfeeds(url)
+        #feeds += getfeed(url)
+        feeds.append(getfeed(url))
+
+    for feed in feeds:
+        print(feed)
 
 if __name__ == '__main__':
     main()
