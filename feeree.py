604deff2dda8ee4ac76ef36a54b7924fad04650d
Michael Wagoner <mwwagoner@gmail.com>
2025-05-03 10:58:21 -0400

    Add the summary field

--- feeree.py
+++ feeree.py
@@ -1,5 +1,5 @@
 import feedparser
-
+# TODO: Add a way to save individual entries from a feed to revisit later
 urls = ['https://thefp.com/feed', 'https://www.wired.com/feed/rss', 'https://standardebooks.org/feeds/rss/new-releases'] 
 
 def getfeeds(feedurl):
@@ -16,6 +16,7 @@ def getfeeds(feedurl):
         print(str(entrycount) + ") " + entry['title'])
         print(entry['published'])
         print(entry['link'])
+        print(entry['summary'])
         print("---")
         entrycount += 1
     
